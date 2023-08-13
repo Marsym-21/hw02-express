87d39f5d6887fe6bd975cd4497234c7902288488
Marsym-21 <mini.garnett.21@gmail.com>
2023-08-13 22:00:07 +0200

    HW06

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,11 +1,13 @@
 import { ctrlWrapper } from "../decorators/index.js";
-import { HttpError } from "../helpers/index.js";
+import { HttpError, transport } from "../helpers/index.js";
 import User from "../models/users/user.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+
+import { nanoid } from "nanoid";
 import "dotenv/config";
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, BASE_URL } = process.env;
 
 const signup = async (req, res) => {
 	const { email, password } = req.body;
@@ -14,11 +16,66 @@ const signup = async (req, res) => {
 		throw HttpError(409, "Email in use");
 	}
 	const hashPassword = await bcrypt.hash(password, 10);
-	const newUser = await User.create({ ...req.body, password: hashPassword });
+	const verificationToken = nanoid();
+	const newUser = await User.create({ ...req.body, password: hashPassword, verificationToken });
+
+	const verifyEmail = {
+		from: "makson.21@meta.ua",
+		to: email,
+		subject: "Verify email",
+		html: `<a href="${BASE_URL}/api/auth/verify/${verificationToken}" target=_"blanck">Click verify email</a>`,
+	};
+
+	transport
+		.sendMail(verifyEmail)
+		.then(() => console.log("Email send success"))
+		.catch(error => console.log(`${error.message}`));
 
 	res.status(201).json({
 		email: newUser.email,
 		subscription: newUser.subscription,
+	});
+};
+
+const verify = async (req, res) => {
+	const { verificationToken } = req.params;
+
+	const user = await User.findOne({ verificationToken });
+	if (!user) {
+		throw HttpError(404, "User not found");
+	}
+	await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: "" });
+
+	res.json({
+		message: "Verification successful",
+	});
+};
+
+const resendVerifyEmail = async (req, res) => {
+	const { email } = req.body;
+	const user = await User.findOne({ email });
+
+	if (!user) {
+		throw HttpError(404, "missing required field email");
+	}
+	if (user.verify) {
+		throw HttpError(400, "Verification has already been passed");
+	}
+
+	const verifyEmail = {
+		from: "makson.21@meta.ua",
+		to: email,
+		subject: "Verify email",
+		html: `<a href="${BASE_URL}/api/auth/verify/${user.verificationToken}" target=_"blanck">Click verify email</a>`,
+	};
+
+	transport
+		.sendMail(verifyEmail)
+		.then(() => console.log("Email send success"))
+		.catch(error => console.log(`${error.message}`));
+
+	res.json({
+		message: "Verification email sent",
 	});
 };
 
@@ -72,7 +129,9 @@ const signout = async (req, res) => {
 
 export default {
 	signup: ctrlWrapper(signup),
+	resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
 	signin: ctrlWrapper(signin),
 	getCurrent: ctrlWrapper(getCurrent),
 	signout: ctrlWrapper(signout),
+	verify: ctrlWrapper(verify),
 };
